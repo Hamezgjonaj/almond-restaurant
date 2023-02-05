056ffdd57de6f9beeef58968899d8053e5f018aa
Hamezgjonaj <hanigjonaj@gmail.com>
2023-02-05 12:28:52 +0100

    modified:   angular.json 	new file:   src/app/about/about.component.css 	new file:   src/app/about/about.component.html 	new file:   src/app/about/about.component.spec.ts 	new file:   src/app/about/about.component.ts 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/checkout/checkout.component.css 	new file:   src/app/checkout/checkout.component.html 	new file:   src/app/checkout/checkout.component.spec.ts 	new file:   src/app/checkout/checkout.component.ts 	new file:   src/app/footer/footer.component.css 	new file:   src/app/footer/footer.component.html 	new file:   src/app/footer/footer.component.spec.ts 	new file:   src/app/footer/footer.component.ts 	new file:   src/app/header/header.component.css 	new file:   src/app/header/header.component.html 	new file:   src/app/header/header.component.spec.ts 	new file:   src/app/header/header.component.ts 	new file:   src/app/landing/landing.component.css 	new file:   src/app/landing/landing.component.html 	new file:   src/app/landing/landing.component.spec.ts 	new file:   src/app/landing/landing.component.ts 	new file:   src/app/locations/locations.component.css 	new file:   src/app/locations/locations.component.html 	new file:   src/app/locations/locations.component.spec.ts 	new file:   src/app/locations/locations.component.ts 	new file:   src/app/menu/menu.component.css 	new file:   src/app/menu/menu.component.html 	new file:   src/app/menu/menu.component.spec.ts 	new file:   src/app/menu/menu.component.ts 	new file:   src/app/reservations/reservations.component.css 	new file:   src/app/reservations/reservations.component.html 	new file:   src/app/reservations/reservations.component.spec.ts 	new file:   src/app/reservations/reservations.component.ts 	new file:   src/app/second-page/second-page.component.css 	new file:   src/app/second-page/second-page.component.html 	new file:   src/app/second-page/second-page.component.spec.ts 	new file:   src/app/second-page/second-page.component.ts 	new file:   src/environments/environment.prod.ts 	new file:   src/environments/environment.ts 	modified:   src/index.html

---
+++ src/app/locations/locations.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+// import { restaurant_locations } from '../../../src/app/locations/locations';
+
+// declare let L;
+
+@Component({
+  selector: 'app-locations',
+  templateUrl: './locations.component.html',
+  styleUrls: ['./locations.component.css']
+})
+export class LocationsComponent implements OnInit {
+
+  constructor() {
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  // locations = restaurant_locations
+
+  // ngOnInit(): void {
+
+  //   const map = L.map('map').setView([51.505, -0.09], 13);
+
+  //   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //   }).addTo(map);
+
+  //   for (var x = 0; x < this.locations.length; x++) {
+  //     L.marker([this.locations[x].lat, this.locations[x].lon]).addTo(map)
+  //       .bindPopup(this.locations[x].name)
+  //       .openPopup();
+  //   }
+  // }
+}
